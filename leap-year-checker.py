4f3aec80dde0dea47fc1cbc528180660f3108768
Treion <90452433+Treion@users.noreply.github.com>
2025-03-05 11:44:09 +0600

    Update leap-year-checker.py

--- leap-year-checker.py
+++ leap-year-checker.py
@@ -1,8 +1,8 @@
 # LEAP YEAR CHECKER PYTHON PROGRAM
 # RUBAIT SIR'S HW: Ex: 5
 
-year = int(input(Enter your year: ))
-if year / 4 AND year NOT / 4
+year = int(input("Enter your year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("Leap Year")
 else:
     print("Not Leap Year")
