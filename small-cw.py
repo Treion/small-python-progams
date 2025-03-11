9a50f58f03a04d6446652fc8c9287493072e26f2
Treion <90452433+Treion@users.noreply.github.com>
2025-03-11 16:14:35 +0600

    Add files via upload

---
+++ small-cw.py
@@ -0,0 +1,42 @@
+# Ex: 1
+# A loop that will print 1-10
+
+# for x in range(1,11):
+#         print(x)
+
+# Ex: 2
+
+# for x in range(2,21,2):
+#         print(x)
+
+# Ex: 3
+
+# for x in range(1,11):
+#         print(x*x)
+
+# Ex: 4
+
+# sum = 0
+# for x in range(11)
+#     sum = sum + x
+# print(sum)
+
+# Bonus Ex
+
+# for x in range(0,(7*11),7):
+#     print(x)
+
+# for i in range(1,11,2):
+#     print(i*7)
+
+# Ex: 7
+
+# for x in range(10,0,-1):
+#     print(x)
+
+# Ex: 9
+
+# product = 1
+# for num in range(5,0,-1):
+#     product = product*num
+# print(product)
