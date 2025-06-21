6032a27a82f7295697353a7cfab6f24442cb1a0c
Treion <90452433+Treion@users.noreply.github.com>
2025-06-21 17:47:15 +0600

    completed the entire task

--- zayan.py
+++ zayan.py
@@ -12,14 +12,40 @@
 #         print("even")
 #     else:
 #         print("odd")
-a = int(input("Grade: "))
-if a >=80:
-    print("A")
-elif a >=70 and a<=79:
-    print("B")
-elif a >=60 and a<=69:
-    print("C")
-elif a>=50 and a<=59:
-    print("D")
-else :
-    print("F")
+
+# 3
+# a = int(input("Grade: "))
+# if a >=80:
+#     print("A")
+# elif a >=70 and a<=79:
+#     print("B")
+# elif a >=60 and a<=69:
+#     print("C")
+# elif a>=50 and a<=59:
+#     print("D")
+# else :
+#     print("F")
+
+# 4
+# OpenSesame = "arham1"
+# inputtedPass = input("Enter a password")
+# if OpenSesame == inputtedPass:
+#     print("Access Granted")
+# else:
+#     print("Access Denied")
+
+# 5
+# temp = float(input("Enter your temperature: "))
+# if temp < 20:
+#     print("It's cold")
+# elif temp <= 29:
+#     print("It's warm")
+# else:
+#     print("It's hot")
+
+# 6
+# num = int(input("Enter a number: "))
+# if num >= 1 and num <= 100:
+#     print("In range")
+# else:
+#     print("Out of range")
