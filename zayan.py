97e0122214f24053ab7f5d3707da3f3e4fbd2df0
Treion <90452433+Treion@users.noreply.github.com>
2025-06-21 17:20:33 +0600

    Add files via upload

---
+++ zayan.py
@@ -0,0 +1,25 @@
+# 1
+# a = int(input("Age: "))
+# if a >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("Yure are not eligible to vote")
+
+# 2
+# while True:
+#     a = int(input("Number: "))
+#     if a % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+a = int(input("Grade: "))
+if a >=80:
+    print("A")
+elif a >=70 and a<=79:
+    print("B")
+elif a >=60 and a<=69:
+    print("C")
+elif a>=50 and a<=59:
+    print("D")
+else :
+    print("F")
